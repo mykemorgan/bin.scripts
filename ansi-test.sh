7f0bb0aa4de2109578adb2b6a7a5af957cf9960a
myke morgan <myke_morgan@yahoo.com>
2019-01-02 16:59:03 -0800

    Totally revamp ansi color tester script
    - Add proper shell scripting instead of cut-and-paste.
    - Add better escape code, not a raw escape chatacter, which also can be
      easily changed for environments that don't support it.
    - Add more modifier code examples, like italic and bright color.
    - Add 8-bit ansi color demo.
    - Add definition of 24-bit ansi colors, but not yet complete. Not sure
      if current shell supports it.

--- ansi-test.sh
+++ ansi-test.sh
@@ -1,46 +1,184 @@
 #!/bin/bash
 # Testing ANSI escape sequences in the terminal.
 #
-# Mac terminal settings seem to be 
-# Escape Sequence: [ "C-Q, C-[ in emacs, I think the escape character"
-# End of escape: m
-# Separator: ;
+# Terminal settings seem to be
+# Escape Character: '' (C-Q, C-[ in emacs) is the raw escape character.
+# Some shells have echo -e. If so can use: '\e'
+# Other shells don't recognize \e. If so can use the escape value: '\033'
+#
+# End of colorizing escape: m
+# Separator for colors: ;
 #
 # xterm's have other sequences, used to set window title and whatnot.
-# See prompt_ucm and family in .bash/functions.
+# See prompt_basic and family in bash/functions.
 #
-# More info see:
+# For more info see:
 # http://en.wikipedia.org/wiki/ANSI_escape_code
 
-echo "Normal first [30mthen color 30[0m then back to normal"
-echo "Normal first [31mthen color 31[0m then back to normal"
-echo "Normal first [32mthen color 32[0m then back to normal"
-echo "Normal first [33mthen color 33[0m then back to normal"
-echo "Normal first [34mthen color 34[0m then back to normal"
-echo "Normal first [35mthen color 35[0m then back to normal"
-echo "Normal first [36mthen color 36[0m then back to normal"
-echo "Normal first [37mthen color 37[0m then back to normal"
-echo "Normal first [30;1mthen color 30;1[0m then back to normal"
-echo "Normal first [31;1mthen color 31;1[0m then back to normal"
-echo "Normal first [32;1mthen color 32;1[0m then back to normal"
-echo "Normal first [33;1mthen color 33;1[0m then back to normal"
-echo "Normal first [34;1mthen color 34;1[0m then back to normal"
-echo "Normal first [35;1mthen color 35;1[0m then back to normal"
-echo "Normal first [36;1mthen color 36;1[0m then back to normal"
-echo "Normal first [37;1mthen color 37;1[0m then back to normal"
-echo "Normal first [37;40mthen color 37;40[0m then back to normal"
-echo "Normal first [37;41mthen color 37;41[0m then back to normal"
-echo "Normal first [37;42mthen color 37;42[0m then back to normal"
-echo "Normal first [37;43mthen color 37;43[0m then back to normal"
-echo "Normal first [37;44mthen color 37;44[0m then back to normal"
-echo "Normal first [37;45mthen color 37;45[0m then back to normal"
-echo "Normal first [37;46mthen color 37;46[0m then back to normal"
-echo "Normal first [37;47mthen color 37;46[0m then back to normal"
-echo "Normal first [37;40;1mthen color 37;40;1[0m then back to normal"
-echo "Normal first [37;41;1mthen color 37;41;1[0m then back to normal"
-echo "Normal first [37;42;1mthen color 37;42;1[0m then back to normal"
-echo "Normal first [37;43;1mthen color 37;43;1[0m then back to normal"
-echo "Normal first [37;44;1mthen color 37;44;1[0m then back to normal"
-echo "Normal first [37;45;1mthen color 37;45;1[0m then back to normal"
-echo "Normal first [37;46;1mthen color 37;46;1[0m then back to normal"
-echo "Normal first [37;47;1mthen color 37;47;1[0m then back to normal"
+# esc="\033"
+esc="\e"
+
+######################################################################
+# ANSI color:
+#
+# Styles:
+# ;1 -> bold
+# ;2 -> faded
+# ;3 -> italic
+# ;4 -> underline
+# ;5 -> blink
+# ;6 -> (nothing?)
+# ;7 -> inverse
+
+echo "****** ANSI colors and modifiers ******"
+# echo "Base colors (30-37):"
+for color in {30..37}
+do {
+  echo -ne "${esc}[${color}mcolor ${color}${esc}[0m "
+  for style in {1,2,3,4,7}
+  do {
+    echo -ne "${esc}[${color};${style}mcolor ${color};${style}${esc}[0m "
+  }
+  done
+  echo
+}
+done
+
+# echo "Bright colors (90-97):"
+for color in {90..97}
+do {
+  echo -ne "${esc}[${color}mcolor ${color}${esc}[0m "
+  # 5 is blink, 6 is nothing?
+  for style in {1,2,3,4,7}
+  do {
+    echo -ne "${esc}[${color};${style}mcolor ${color};${style}${esc}[0m "
+  }
+  done
+  echo
+}
+done
+
+# echo "Background colors (40-47):"
+for bkg in {40..47}
+do {
+  echo -ne "${esc}[37;${bkg}mcolor 37;${bkg}${esc}[0m "
+  for style in {1,2,3,4,7}
+  do {
+    echo -ne "${esc}[37;${bkg};${style}mcolor 37;${bkg};${style}${esc}[0m "
+  }
+  done
+  echo
+}
+done
+
+# echo "Bright Background colors (100-107):"
+for bkg in {100..107}
+do {
+  echo -ne "${esc}[37;${bkg}mcolor 37;${bkg}${esc}[0m "
+  for style in {1,2,3,4,7}
+  do {
+    echo -ne "${esc}[37;${bkg};${style}mcolor 37;${bkg};${style}${esc}[0m "
+  }
+  done
+  echo
+}
+done
+
+# Just in case we mucked things up
+echo -ne "${esc}[0m"
+
+######################################################################
+# 8-bit color method:
+#
+# As 256-color lookup tables became common on graphic cards, escape sequences
+# were added to select from a pre-defined set of 256 colors:
+#
+#    ESC[ 38;5;<n> m Select foreground color
+#    ESC[ 48;5;<n> m Select background color
+#      0-  7:  standard colors (as in ESC [ 30–37 m)
+#      8- 15:  high intensity colors (as in ESC [ 90–97 m)
+#     16-231:  6 × 6 × 6 cube (216 colors): 16 + 36 × r + 6 × g + b (0 ≤ r, g, b ≤ 5)
+#    232-255:  grayscale from black to white in 24 steps
+######################################################################
+
+# 8 basic colors, 8 bright/intense versions
+echo "****** 8-bit colors ******"
+numcolors=8
+for intensity in {0,1}
+do {
+  for color in {0..7}
+  do {
+    ((shownum=$numcolors * ${intensity} + $color))
+    echo -en "${esc}[0;48;5;${shownum}m color ${shownum} ${esc}[0m"
+  }
+  done
+  echo
+}
+done
+
+# Note: Much angst goes into the orderiung of these :)
+# Yes the numbers are "out of reading order" when printed, but the
+# red / blue / green and green / blue / red loop nestings both make
+# the color "cube" more smooth and apparent.
+# red / green / blue nesting puts numbers in L to R top to bottom order.
+redoffset=36
+greenoffset=6
+cubestart=16
+for green in {0..5}
+do {
+  for blue in {0..5}
+  do {
+    for red in {0..5}
+    do {
+      ((bg=$cubestart + $redoffset * $red + $greenoffset * $green + $blue))
+      # Invert the text against the background: looks a bit wonky
+      # ((textcolor=247-$bg))
+
+      # Make an estimate of intensity for the foreground?
+      textcolor=0
+      if ((($green + $red) < 4)) ; then
+        textcolor=15
+      fi
+      echo -en "${esc}[48;5;${bg}m${esc}[38;5;${textcolor}m color ${bg} ${esc}[0m"
+    }
+    done
+    echo
+  }
+  done
+}
+done
+
+# 24 steps of greyscale
+bwoffset=232
+for grey in {0..23}
+do {
+   ((bg=$bwoffset + $grey))
+   echo -en "${esc}[0;48;5;${bg}m ${bg}${esc}[0m"
+}
+done
+
+echo
+
+# Just in case we mucked things up
+echo -ne "${esc}[0m"
+
+######################################################################
+# TODO:
+# 24-bit
+#
+# As "true color" graphic cards with 16 to 24 bits of color became common,
+# Xterm,[19] KDE's Konsole,[26] as well as all libvte based terminals[27]
+# (including GNOME Terminal) support 24-bit foreground and background color
+# setting,
+#
+#    ESC[ 38;2;<r>;<g>;<b> m Select RGB foreground color
+#    ESC[ 48;2;<r>;<g>;<b> m Select RGB background color
+
+echo "****** 24-bit colors under construction ******"
+
+echo -en "${esc}[38;2;50m${esc}[48;2;24;24;24;m color 64.0.0 ${esc}[0m"
+echo -en "${esc}[48;2;64;0;0m${esc}[38;2;0;0;0m color 64.0.0 ${esc}[0m"
+echo -en "${esc}[48;2;0;64;0m${esc}[38;2;0;0;0m color 0.64.0 ${esc}[0m"
+echo -en "${esc}[48;2;0;0;64m${esc}[38;2;0;0;0m color 0.0.64 ${esc}[0m"
+echo -en "${esc}[48;2;100;100;100m${esc}[38;2;0;0;0m color 100.100.100 ${esc}[0m"
+echo
